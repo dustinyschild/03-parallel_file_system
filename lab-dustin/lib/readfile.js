9ad78bec6eab8076adf73f2a44fd227e2a7dc4ae
Dustin <dyelland@gmail.com>
2017-08-15 16:52:20 -0500

    added test for knockknock.txt

--- lab-dustin/lib/readfile.js
+++ lab-dustin/lib/readfile.js
@@ -2,15 +2,15 @@
 
 const { readFile } = require('fs');
 
-function readData(file/*, callback*/) {
+exports.readData = function(file, callback) {
   readFile(`${__dirname}/../assets/${file}`, function(err, buff){
-    /*if (err) { callback(err); }*/
+    if (err) { callback(err); }
 
     console.log(`Data from ${__dirname}/../assets/${file}`);
-    console.log('Buffer ', buff);
+    console.log(buff.toString('hex').slice(0,8));
 
-    //if(callback) { callback(null, buff); }
+    if(callback) { callback(null, buff.toString('hex').slice(0,8)); }
   });
-}//;
+};
 
-readData('yomamma.txt');
+//readData('yomamma.txt');
--- lab-dustin/test/readfile.test.js
+++ lab-dustin/test/readfile.test.js
@@ -4,11 +4,22 @@ const read = require('../lib/readfile');
 
 describe('Read File Module', function(){
   describe('#readFile()', function(){
-    it('should read the yomammafile', function(){
+    it('should return first 8 bytes in hex format of yomamma.js', function(done){
       read.readData('yomamma.txt', function(err, data) {
-        //tests with callback data from callback
+        //tests with data from callback
         assert.ifError(err);
-        assert.equal();
+        assert.strictEqual(data, '596f206d');
+
+        done();
+      });
+    });
+    it('should return first 8 bytes in hex format of knockknock.js', function(done){
+      read.readData('knockknock.txt', function(err, data) {
+        //tests with data from callback
+        assert.ifError(err);
+        assert.strictEqual(data, '4b6e6f63');
+
+        done();
       });
     });
   });
